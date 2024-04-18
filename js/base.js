50b041314143f7b4e3ea30be2d51f0f4e6f4da0d
Somnyan <somnyan.interface@gmail.com>
2024-04-18 22:44:03 +0900

    added favicon

--- js/base.js
+++ js/base.js
@@ -1,16 +1,14 @@
-import barba from '@barba/core';
-
 barba.init({
-    transitions: [{
-      leave(data) {
-        return gsap.to(data.current.container, {
-          x:"100%"
-        });
-      },
-      enter(data) {
-        gsap.from(data.next.container, {
-          x: "-100%"
-        });
-      }
-    }]
-  });
+  transitions: [{
+    leave(data) {
+      return gsap.to(data.current.container, {
+        x:"100%"
+      });
+    },
+    enter(data) {
+      gsap.from(data.next.container, {
+        x: "-100%"
+      });
+    }
+  }]
+});
